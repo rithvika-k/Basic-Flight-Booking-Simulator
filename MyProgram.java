ecbefb7f914a4874c2d2ad7d5040a7a9f95d3421
Rithvika Kathroju <91027820+rithvika-k@users.noreply.github.com>
2024-01-01 02:16:34 -0500

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -1,23 +1,5 @@
-/*
-Rithvika Kathroju 
-9/27/2022
-ICS 4U1-01
-
-Unit 1 Assignment
-
-Flight Booking System
-- allows the user to book a ticket to any of the travel destinations we offer, they are able to input personal information 
-  and book a ticket. This will then be outputted to the user in the form of a booking receipt
-
-NOTES README!!
-- implemented arrays and sorting arrays
-- implemented try and catch blocks 
-- implemented classes:
-  --- a Ticket class which acts as a storage to hold key attributes of any ticket object created
-      although the Ticket class isnt used as much, its mainly to keep the code organized and make working with
-      the attributes of any ticket object easier in the future (Future assignments as i build on this program)
-*/
-
+// Rithvika Kathroju 
+// 9/27/2022
 
 import java.util.Scanner; //import scanner class
 import java.util.Arrays; //import arrays class for the sort method
